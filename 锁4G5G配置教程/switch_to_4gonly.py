113c6d45d136fe7c303a64d46c82e68e081e77b6
openwrtkitty <112697407+openwrtkitty@users.noreply.github.com>
2022-12-05 13:30:05 +0800

    Add files via upload

---
+++ 锁4G5G配置教程/switch_to_4gonly.py
@@ -0,0 +1,31 @@
+import paramiko
+import sys
+import time
+
+ssh = paramiko.SSHClient()
+# 允许连接不在know_hosts文件中的主机
+ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+
+password = sys.argv[1]
+
+# 建立连接
+ssh.connect("172.16.0.1", username="root", port=22, password=password)
+
+shell = ssh.invoke_shell()
+shell.send('picocom -b 115200 /dev/ttyUSB3\n')
+time.sleep(1)
+print(shell.recv(10000).decode("utf8"))
+
+shell.send('\r')
+shell.send('AT+QNWPREFCFG="mode_pref",LTE\r')
+shell.send('\r')
+time.sleep(3)
+print(shell.recv(10000).decode("utf8"))
+
+shell.send('\x01')
+shell.send('\x11')
+time.sleep(1)
+print(shell.recv(10000).decode("utf8"))
+
+# 关闭连接
+ssh.close()
